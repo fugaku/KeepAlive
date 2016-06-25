f9e32ace31b6f65b88f33d095564172d87bf1c3a
Daniel <Daniel@FUGAKU>
2016-06-26 05:57:41 +0700

    Add project files.

---
+++ KeepAlive/PingJob.cs
@@ -0,0 +1,23 @@
+﻿using Quartz;
+using System.Net;
+
+namespace KeepAlive
+{
+    public class PingJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers["Content-Type"] = "application/json";
+                try
+                {
+                    var result = client.DownloadString("http://cashwebapi.apphb.com/api/test");
+                }
+                catch
+                {
+                }
+            }
+        }
+    }
+}
---
+++ KeepAlive/Program.cs
@@ -0,0 +1,25 @@
+﻿using Quartz;
+using Quartz.Impl;
+
+namespace KeepAlive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // construct a scheduler
+            var schedulerFactory = new StdSchedulerFactory();
+            var scheduler = schedulerFactory.GetScheduler();
+            scheduler.Start();
+
+            var job = JobBuilder.Create<PingJob>().Build();
+
+            var trigger = TriggerBuilder.Create()
+                            .WithSimpleSchedule(x => x.WithIntervalInMinutes(19).RepeatForever())
+                            .StartNow()
+                            .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
